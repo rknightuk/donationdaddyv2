4498fd979bbe19e8a734f0dc428c247642d5ae1d
Robb Knight <desk@rknight.me>
2025-08-24 22:31:28 +0100

    Weee

--- routes/web.php
+++ routes/web.php
@@ -6,27 +6,45 @@ use Illuminate\Support\Facades\Route;
 
 Route::domain('coinme.dad')->group(function () {
     Route::get('/', DaddyController::class . '@coin');
+    Route::get('/dy', DaddyController::class . '@coin');
 });
 
-Route::domain('coin.rknight.me', DaddyController::class . '@coin');
+Route::domain('coin.rknight.me')->group(function () {
+    Route::get('/', DaddyController::class . '@coin');
+    Route::get('/dy', DaddyController::class . '@coin');
+});
+
+Route::domain('deskmat.help')->group(function () {
+    Route::get('/', DaddyController::class . '@deskmat');
+});
+
+Route::domain('donationtreats.rknight.me')->group(function () {
+    Route::get('/', DaddyController::class . '@home');
+});
+
+Route::domain('septembed.rknight.me')->group(function () {
+    Route::get('/', DaddyController::class . '@septembed');
+});
+
+Route::domain('hathelp.rknight.me')->group(function () {
+    Route::get('/', DaddyController::class . '@bag');
+});
+
+Route::domain('backpackhelp.rknight.me')->group(function () {
+    Route::get('/', DaddyController::class . '@bag');
+});
 
 Route::get('/', DaddyController::class . '@home');
-
 Route::get('/coin', DaddyController::class . '@coin');
 
 Route::get('/deskmat', DaddyController::class . '@deskmat');
-Route::domain('deskmat.help', DaddyController::class . '@deskmat');
 
 Route::get('/treats', DaddyController::class . '@home');
-Route::domain('donationtreats.rknight.me', DaddyController::class . '@home');
 
 Route::get('/septembed', DaddyController::class . '@septembed');
-Route::domain('septembed.rknight.me', DaddyController::class . '@septembed');
 Route::get('/sj', TiltifyController::class . '@embed');
 
 Route::get('/backpack', DaddyController::class . '@bag');
-Route::domain('hathelp.rknight.me', DaddyController::class . '@bag');
-Route::domain('backpackhelp.rknight.me', DaddyController::class . '@bag');
 
 Route::get('api/campaigns', TiltifyController::class . '@campaigns');
 Route::get('api/campaign', TiltifyController::class . '@campaign');
